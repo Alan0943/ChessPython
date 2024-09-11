0d44bbe8633438a3811313535f98c437479f03ee
Alan <alanskrypek2@gmail.com>
2024-09-11 16:32:06 -0400

    Implemented logic for pawn, bishop, knight, rook, and queen, need to work on king and check/checkmate logic

--- Board.py
+++ Board.py
@@ -44,7 +44,24 @@ pieces_dict = {
     (350, 300): 'WPawn', (350, 350): 'WRook'
 }
 
-
+c_coord_dict = {
+    0: 'a',
+    50: 'b',
+    100: 'c',
+    150: 'd',
+    200: 'e',
+    250: 'f',
+    300: 'g',
+    350: 'h'
+}
+r_coord_dict = {0: 8,
+                50: 7,
+                100: 6,
+                150: 5,
+                200: 4,
+                250: 3,
+                300: 2,
+                350: 1}
 # sets up images, and board
 def initialize_board():
     # name of the images
@@ -101,8 +118,157 @@ def update_pieces_dict(fc, fr, c, r):
     screen.blit(images_dict[piece_name], (c*block_size, r * block_size))
 
 
-def legal_moves():
+def legal_moves(fc, fr, c, r):
+    og_piece = pieces_dict[(fc * block_size, fr * block_size)]
+    target_piece = [0]
+
+    if len(pieces_dict[(c * block_size, r * block_size)]) > 0:
+        target_piece = pieces_dict[(c * block_size, r * block_size)]
+
+    # Makes it so if pieces try to attack the same color, fails
+    if og_piece[0] == "W" and target_piece[0] == "W":
+        return False
+    if og_piece[0] == "B" and target_piece[0] == "B":
+        return False
+
+    # Move logic for white pawns - add en passant
+    if og_piece == "WPawn":
+
+        # checks when taking a piece, uses dict coordinate system
+        if len(target_piece) > 2 and r_coord_dict[r * block_size] - r_coord_dict[fr * block_size] == 1:
+            if abs(fc * block_size - c * block_size) == 50:
+                return True
+
+        # prevents the pawn from moving different columns
+        if fc != c:
+            return False
+
+        # prevents from taking a piece when moving forward
+        if len(target_piece) > 2 and fc == c:
+            return False
+
+        # allows double move if on correct row, uses dict coordinate system
+        if r_coord_dict[fr * block_size] == 2 and r_coord_dict[r * block_size] - r_coord_dict[fr * block_size] <= 2 and len(pieces_dict[(fc * block_size, 250)]) < 2:
+            return True
+
+        if r_coord_dict[r * block_size] - r_coord_dict[fr * block_size] >= 2:
+            return False
+
+    # Move logic for black pawns - add en passant
+    elif og_piece == "BPawn":
+        if len(target_piece) > 2 and r_coord_dict[r * block_size] - r_coord_dict[fr * block_size] == -1:
+            if abs(fc * block_size - c * block_size) == 50:
+                return True
+
+        # prevents the pawn from moving different columns
+        if fc != c:
+            return False
+
+        # prevents from taking a piece when moving forward
+        if len(target_piece) > 2 and fc == c:
+            return False
+
+        if r_coord_dict[fr * block_size] == 7 and r_coord_dict[r * block_size] - r_coord_dict[fr * block_size] >= -2 and len(pieces_dict[(fc * block_size, 100)]) < 2:
+            return True
+
+        if r_coord_dict[r * block_size] - r_coord_dict[fr * block_size] <= -2:
+            return False
+
+    elif og_piece == "WBishop" or og_piece == "BBishop":
+        # Bishop moves diagonally, so the absolute difference between file and rank must be the same
+        if abs(fc * block_size - c * block_size) != abs(fr * block_size - r * block_size):
+            return False
+
+        # Determine the direction of the movement
+        file_direction = 1 if c > fc else -1
+        rank_direction = 1 if r > fr else -1
+
+        # Check all squares between the start and end positions to ensure there are no pieces in the way
+        for step in range(1, abs(c - fc)):
+            intermediate_file = fc + step * file_direction
+            intermediate_rank = fr + step * rank_direction
+
+            if len(pieces_dict[(intermediate_file * block_size, intermediate_rank * block_size)]) > 0:
+                return False
+
+        return True
+
+    elif og_piece == "WKnight" or og_piece == "BKnight":
+
+        # allows for 2 up, 1 over
+        if abs(fr * block_size - r * block_size) == 100 and abs(fc * block_size - c * block_size) == 50:
+            return True
+
+        # allows for 2 over, 1 up
+        if abs(fr * block_size - r * block_size) == 50 and abs(fc * block_size - c * block_size) == 100:
+            return True
+
+        return False
+
+    elif og_piece == "WQueen" or og_piece == "BQueen":
+
+        # diagonal
+        if abs(fc * block_size - c * block_size) == abs(fr * block_size - r * block_size):
+
+            file_direction = 1 if c > fc else -1
+            rank_direction = 1 if r > fr else -1
+
+            # Checks for pieces diagonally
+            for step in range(1, abs(c - fc)):
+                intermediate_file = fc + step * file_direction
+                intermediate_rank = fr + step * rank_direction
+
+                if len(pieces_dict[(intermediate_file * block_size, intermediate_rank * block_size)]) > 0:
+                    return False
+
+            return True
+
+        # vertical
+        if fc == c:
+
+            if fr > r:
+                for step in range(r+1, fr):
+                    if len(pieces_dict[(c * block_size, step * block_size)]) > 0:
+                        return False
+
+            if fr < r:
+                for step in range(fr+1, r):
+                    if len(pieces_dict[(c * block_size, step * block_size)]) > 0:
+                        return False
+
+            return True
+
+        # horizontal
+        if fr == r:
+
+            if c > fc:
+                for step in range(fc+1, c):
+                    if len(pieces_dict[(step * block_size, r * block_size)]) > 0:
+                        return False
+
+            if c < fc:
+                for step in range(c+1, fc):
+                    if len(pieces_dict[(step * block_size, r * block_size)]) > 0:
+                        return False
+
+            return True
+
+        else:
+            return False
+
+
+
+    return True
+
+
+def king_check(self, piece):
     pass
+
+
+def move_notation(fc, fr, c, r):
+    piece_name = pieces_dict[(fc * block_size, fr * block_size)]
+    message = f"{piece_name} from {c_coord_dict[fc * block_size]}{r_coord_dict[fr * block_size]} to {c_coord_dict[c * block_size]}{r_coord_dict[r * block_size]}"
+    return message
 
 
 first_col = None
@@ -110,7 +276,11 @@ first_row = None
 glow_col = None
 glow_row = None
 
+turn = 0
+black_piece = None
+
 initialize_board()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -122,14 +292,47 @@ while True:
             col = event.pos[0] // 50
             row = event.pos[1] // 50
 
-            print(col, row)
+            # print(col, row)
 
             # Skips if a piece is not clicked on first move
             if pieces_dict[(col * block_size, row * block_size)] == '' and first_col is None:
                 continue
 
+            # This is to prevent an IndexError whenever finding black_piece
+            if len(pieces_dict[(col * block_size, row * block_size)]) > 0:
+                black_piece = pieces_dict[(col * block_size, row * block_size)][0]
+                # Skips if first move is black on white's turn
+                if black_piece == 'B' and first_row is None and turn % 2 == 0:
+                    continue
+                # Skips if first move is white on black's turn
+                if black_piece == 'W' and first_row is None and turn % 2 == 1:
+                    continue
+
+            # Checks and then if approved, moves the pieces, and the turn sequence
             if first_col is not None:
+
+                # prevents you from moving to the same spot
+                if first_col == col and first_row == row:
+                    continue
+
+                if not king_check(1, black_piece):
+                    print("Woah that's not valid")
+                    continue
+                
+                if not legal_moves(first_col, first_row, col, row):
+                    print("Woah that's not valid")
+                    continue
+
+                print(move_notation(first_col, first_row, col, row))
                 update_pieces_dict(first_col, first_row, col, row)
+
+                turn += 1
+                if turn % 2 == 0:
+                    print("Its white's turn")
+
+                else:
+                    print("Its black's turn")
+
                 first_col = None
                 first_row = None
                 continue
